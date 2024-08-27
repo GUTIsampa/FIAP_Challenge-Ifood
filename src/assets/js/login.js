a6173f473b3212b8616fb31b63bce8f920dfb4d4
Augusto Sampaio <augusto_lbs@hotmail.com>
2024-08-26 22:31:23 -0300

    Ifood
    Pasta principal do Challange Ifood

---
+++ src/assets/js/login.js
@@ -0,0 +1,14 @@
+function validaForm() {
+    document.querySelector(".mensagem-erro").style.display = "none";
+    if (document.getElementById("email").value.length == 0) {
+        document.getElementById("email").focus();
+        document.getElementById('error-message1').style.display = "block";
+        return false;
+    }
+    if (document.getElementById("senha").value.length == 0) {
+        document.getElementById("senha").focus();
+        document.getElementById('error-message2').style.display = 'block';
+        return false;
+    }
+    window.location.href = "./assets/pages/PainelFinanceiro.html";
+}
