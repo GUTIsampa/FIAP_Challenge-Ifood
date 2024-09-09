7e478e5bcca84f1efb9f4a0bbf730492bbac25e7
WesleyMR1 <WesleyMR012@gmail.com>
2024-09-09 17:34:20 -0300

    correção dos scripts das paginas carregadas

--- src/assets/js/Saldo.js
+++ src/assets/js/Saldo.js
@@ -1,1 +1,1 @@
-console.log('saldo');
+document.getElementById("btn1").addEventListener("click", () => {console.log("clicou no botão")})
--- src/assets/js/pageScript.js
+++ src/assets/js/pageScript.js
@@ -19,17 +19,45 @@ document.addEventListener('DOMContentLoaded', () => {
                 return response.text();
             })
             .then(data => {
-                contentDiv.innerHTML = data;
-                updateActiveLink(page);
-                // Aqui você pode adicionar quaisquer scripts específicos para a página carregada
-            }).then(() => {
+                const contentDiv = document.getElementById('content');
+    
+                // Limpa o conteúdo antigo, incluindo scripts
+                contentDiv.innerHTML = '';
+    
+                // Cria um elemento temporário para parsear a nova página
+                const tempDiv = document.createElement('div');
+                tempDiv.innerHTML = data;
+    
+                // Adiciona o novo conteúdo ao 'content'
+                contentDiv.innerHTML = tempDiv.innerHTML;
+    
+                // Adiciona os scripts da nova página dentro do 'content'
+                const scripts = tempDiv.querySelectorAll('script');
+                scripts.forEach(script => {
+                    const newScript = document.createElement('script');
+                    if (script.src) {
+                        // Se o script tiver um src, cria um novo elemento script com src
+                        newScript.src = script.src;
+                    } else {
+                        // Se o script tiver código inline, adiciona o código ao novo elemento script
+                        newScript.textContent = script.textContent;
+                    }
+                    // Adiciona o novo script ao 'contentDiv'
+                    contentDiv.appendChild(newScript);
+                });
+    
+                // Atualiza o título
                 document.getElementById("titulo").textContent = `${page}`;
+    
+                updateActiveLink(page);
             })
             .catch(error => {
-                contentDiv.innerHTML = `<p class="text-danger">Ocorreu um erro ao carregar o conteúdo.</p>`;
+                document.getElementById('content').innerHTML = `<p class="text-danger">Ocorreu um erro ao carregar o conteúdo.</p>`;
                 console.error(error);
             });
     }
+    
+    
 
 
     
