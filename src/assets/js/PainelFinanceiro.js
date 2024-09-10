22f38fe73a82708b9f7964380fbfc421bb07a4eb
Douglas Dias <douglas.diasesilva@gmail.com>
2024-09-09 21:29:29 -0300

    Sprint-5 Challenge Ifood

--- src/assets/js/PainelFinanceiro.js
+++ src/assets/js/PainelFinanceiro.js
@@ -1,3 +1,35 @@
+// Seleciona os dropdowns
+const mesesDropdown = document.querySelector('.mesInfo .dropdown:nth-child(1)');
+const anosDropdown = document.querySelector('.mesInfo .dropdown:nth-child(2)');
+
+// Função para atualizar o título com base na seleção
+function updateTitle(dropdown, selectedOption) {
+  dropdown.querySelector('.selectedOption').innerHTML = selectedOption;
+}
+
+// Adiciona os eventos de clique nos itens de cada dropdown
+mesesDropdown.querySelectorAll('.dropdown-item').forEach(item => {
+  item.addEventListener('click', function() {
+    updateTitle(mesesDropdown, `<i class="fa-solid fa-calendar-days"></i> ${this.textContent}`);
+  });
+});
+
+anosDropdown.querySelectorAll('.dropdown-item').forEach(item => {
+  item.addEventListener('click', function() {
+    updateTitle(anosDropdown, this.textContent);
+  });
+});
+
+const dataAtual = new Date();
+const mesAtual = dataAtual.getMonth();
+const meses = [
+    "JANEIRO", "FEVEREIRO", "MARÇO", "ABRIL", "MAIO", "JUNHO",
+    "JULHO", "AGOSTO", "SETEMBRO", "OUTUBRO", "NOVEMBRO", "DEZEMBRO"
+  ];
+document.getElementById("mesSelecionado").innerHTML = `<i class="fa-solid fa-calendar-days"></i> ${meses[mesAtual]}`;
+const anoAtual = dataAtual.getFullYear();
+document.getElementById("anoSelecionado").innerHTML = `${anoAtual}`;
+
 
 
 
