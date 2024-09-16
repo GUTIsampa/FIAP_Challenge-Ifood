85215e34e53b532769a3711c15e80aeeab58f33a
WesleyMR1 <WesleyMR012@gmail.com>
2024-09-16 16:26:24 -0300

    debug no dropdown dos meses

--- src/assets/js/PainelFinanceiro.js
+++ src/assets/js/PainelFinanceiro.js
@@ -1,34 +1,39 @@
 // Seleciona os dropdowns
-const mesesDropdown = document.querySelector('.mesInfo .dropdown:nth-child(1)');
-const anosDropdown = document.querySelector('.mesInfo .dropdown:nth-child(2)');
+if (typeof mesesDropdown === "undefined" && typeof anosDropdown === "undefined") {
+  let mesesDropdown = document.querySelector('.mesInfo .dropdown:nth-child(1)');
+  let anosDropdown = document.querySelector('.mesInfo .dropdown:nth-child(2)');
+  let dataAtual = new Date();
+  let mesAtual = dataAtual.getMonth();
+  let meses = [
+    "JANEIRO", "FEVEREIRO", "MARÇO", "ABRIL", "MAIO", "JUNHO",
+    "JULHO", "AGOSTO", "SETEMBRO", "OUTUBRO", "NOVEMBRO", "DEZEMBRO"
+  ];
+  document.getElementById("mesSelecionado").innerHTML = `<i class="fa-solid fa-calendar-days"></i> ${meses[mesAtual]}`;
+  let anoAtual = dataAtual.getFullYear();
+  document.getElementById("anoSelecionado").innerHTML = `${anoAtual}`;
+  // Adiciona os eventos de clique nos itens de cada dropdown
+  mesesDropdown.querySelectorAll('.dropdown-item').forEach(item => {
+    item.addEventListener('click', function () {
+      updateTitle(mesesDropdown, `<i class="fa-solid fa-calendar-days"></i> ${this.textContent}`);
+    });
+  });
+
+  anosDropdown.querySelectorAll('.dropdown-item').forEach(item => {
+    item.addEventListener('click', function () {
+      updateTitle(anosDropdown, this.textContent);
+    });
+  });
+
+}
+
 
 // Função para atualizar o título com base na seleção
 function updateTitle(dropdown, selectedOption) {
   dropdown.querySelector('.selectedOption').innerHTML = selectedOption;
 }
 
-// Adiciona os eventos de clique nos itens de cada dropdown
-mesesDropdown.querySelectorAll('.dropdown-item').forEach(item => {
-  item.addEventListener('click', function() {
-    updateTitle(mesesDropdown, `<i class="fa-solid fa-calendar-days"></i> ${this.textContent}`);
-  });
-});
 
-anosDropdown.querySelectorAll('.dropdown-item').forEach(item => {
-  item.addEventListener('click', function() {
-    updateTitle(anosDropdown, this.textContent);
-  });
-});
 
-const dataAtual = new Date();
-const mesAtual = dataAtual.getMonth();
-const meses = [
-    "JANEIRO", "FEVEREIRO", "MARÇO", "ABRIL", "MAIO", "JUNHO",
-    "JULHO", "AGOSTO", "SETEMBRO", "OUTUBRO", "NOVEMBRO", "DEZEMBRO"
-  ];
-document.getElementById("mesSelecionado").innerHTML = `<i class="fa-solid fa-calendar-days"></i> ${meses[mesAtual]}`;
-const anoAtual = dataAtual.getFullYear();
-document.getElementById("anoSelecionado").innerHTML = `${anoAtual}`;
 
 
 
--- src/assets/js/Saldo.js
+++ src/assets/js/Saldo.js
@@ -1,1 +1,1 @@
-document.getElementById("btn1").addEventListener("click", () => {console.log("clicou no botão")})
+console.log("PG saldo carregada")
