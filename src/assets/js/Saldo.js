9540379d70f132f036000d1ad6e49105541177a0
Douglas Dias <douglas.diasesilva@gmail.com>
2024-09-16 22:05:27 -0300

    A responsividade do projeto como um todo foi finalizada.

--- src/assets/js/Saldo.js
+++ src/assets/js/Saldo.js
@@ -1,1 +1,21 @@
-console.log("PG saldo carregada")
+if(typeof valorTabela === "undefined"){
+    let valorTabela = document.querySelectorAll(".valorTabela");
+    valorTabela.forEach(valor => {
+        if (valor.textContent.trim().startsWith("-")) {
+            valor.classList.add('negativos')
+            console.log("Classe negativa");
+        }else{
+            valor.classList.add("positivos")
+            console.log("Classe negativa");
+        }
+    });
+}else{
+    valorTabela.forEach(valor => {
+        if (valor.textContent.trim().startsWith("-")) {
+            valor.classList.add('negativos')
+            console.log("Classe negativa");
+        }else{
+            valor.classList.add("positivos")
+            console.log("Classe positiva");
+        }
+})}
